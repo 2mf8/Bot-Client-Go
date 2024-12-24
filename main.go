3807532e8c72e63b4c95e5641e650ebcd51e9944
一仝 <chun5060@outlook.com>
2024-12-24 23:19:43 +0800

    update

--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/2mf8/Bot-Client-Go/safe_ws"
 
 	bot "github.com/2mf8/Better-Bot-Go"
 	"github.com/2mf8/Better-Bot-Go/dto"
+	"github.com/2mf8/Better-Bot-Go/dto/keyboard"
 	"github.com/2mf8/Better-Bot-Go/openapi"
 	"github.com/2mf8/Better-Bot-Go/token"
 	"github.com/2mf8/Better-Bot-Go/webhook"
@@ -29,14 +32,76 @@ func main() {
 	b, _ := json.Marshal(as)
 	fmt.Println("配置", string(b))
 	safe_ws.GroupAtMessageEventHandler = func(appid string, event *dto.WSPayload, data *dto.WSGroupATMessageData) error {
-		log.Info(data.Content, data.GroupId)
-		/* ctx := context.WithValue(context.Background(), "key", "value")
-		newMsg := &dto.GroupMessageToCreate{
-			Content: "测试",
-			MsgID:   data.MsgId,
-			MsgType: 0,
+		content := strings.TrimSpace(data.Content)
+		log.Info(data.Content, data.GroupId, " -",content)
+		if content == "kb" {
+			ctx := context.WithValue(context.Background(), "key", "value")
+			/* rows := keyboard.CustomKeyboard{} */
+			/* kb := gkb.Builder().
+			TextButton("测试", "已测试", "成功", false, true).
+			UrlButton("爱魔方吧", "一仝", "https://2mf8.cn", false, true).
+			SetRow().
+			TextButton("测试", "已测试", "成功", false, true).
+			SetRow()
+			b, _:= json.Marshal(kb)
+			json.Unmarshal(b, &rows) */
+			fmt.Println("测试")
+			Apis[appid].PostGroupMessage(ctx, data.GroupId, &dto.C2CMessageToCreate{
+				Keyboard: &keyboard.MessageKeyboard{
+					ID: "101981675_1735044770",
+				},
+				MsgType: dto.C2CMsgTypeMarkdown,
+				MsgID:   data.MsgId,
+			})
 		}
-		Apis[appid].PostGroupMessage(ctx, data.GroupId, newMsg) */
+		return nil
+	}
+	safe_ws.C2CMessageEventHandler = func(appid string, event *dto.WSPayload, data *dto.WSC2CMessageData) error {
+		content := strings.TrimSpace(data.Content)
+		log.Info(data.Content, data.Author.UserOpenId, " -",content)
+		if content == "kb" {
+			ctx := context.WithValue(context.Background(), "key", "value")
+			/* rows := keyboard.CustomKeyboard{} */
+			/* kb := gkb.Builder().
+			TextButton("测试", "已测试", "成功", false, true).
+			UrlButton("爱魔方吧", "一仝", "https://2mf8.cn", false, true).
+			SetRow().
+			TextButton("测试", "已测试", "成功", false, true).
+			SetRow()
+			b, _:= json.Marshal(kb)
+			json.Unmarshal(b, &rows) */
+			fmt.Println("测试")
+			Apis[appid].PostC2CMessage(ctx, data.Author.UserOpenId, &dto.C2CMessageToCreate{
+				Keyboard: &keyboard.MessageKeyboard{
+					ID: "101981675_1735044770",
+				},
+				MsgType: dto.C2CMsgTypeMarkdown,
+				MsgID:   data.Id,
+			})
+		}
+		return nil
+	}
+	safe_ws.MessageEventHandler = func(appid string, event *dto.WSPayload, data *dto.WSMessageData) error {
+		if data.Content == "k" {
+			ctx := context.WithValue(context.Background(), "key", "value")
+			Apis[appid].PostMessage(ctx, data.ChannelID, &dto.MessageToCreate{
+				Markdown: &dto.Markdown{
+					CustomTemplateId: "101981675_1735049881",
+				},
+				Keyboard: &keyboard.MessageKeyboard{
+					ID: "101981675_1735044770",
+				},
+			})
+		}
+		return nil
+	}
+	safe_ws.InteractionEventHandler = func(appid string, event *dto.WSPayload, data *dto.WSInteractionData) error {
+		/* fmt.Println(data.ChannelID)
+		ctx := context.WithValue(context.Background(), "key", "value")
+			Apis[appid].PostMessage(ctx, data.ChannelID, &dto.MessageToCreate{
+				Content: "测试",
+				MsgID: data.ID,
+			}) */
 		return nil
 	}
 	select {}
